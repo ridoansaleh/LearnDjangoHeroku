f64f5053b3cb802b798682ffe626b52343e3f031
ridooo <ridooprent@gmail.com>
2018-02-01 14:04:25 +0700

    minor changes

--- app/settings/prod.py
+++ app/settings/prod.py
@@ -33,7 +33,6 @@ DATABASES = {
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
