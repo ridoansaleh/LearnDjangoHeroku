9c74754eabc1ad8bf065d591752654a847e3effb
ridooo <ridooprent@gmail.com>
2018-02-01 14:18:04 +0700

    some changes

--- app/settings/dev.py
+++ app/settings/dev.py
@@ -20,7 +20,7 @@ MIDDLEWARE_CLASSES = (
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
     }
 }
 
